980e0f09066442e0af4fc21d689709189e4aa6b1
Andres Chapeton <andres.chapeton0206@gmail.com>
2023-03-02 23:05:35 -0600

    Create input validations

--- src/modules/TodoForm/index.jsx
+++ src/modules/TodoForm/index.jsx
@@ -18,15 +18,39 @@ const TodoForm = () => {
     <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label>Name:</label>
-        <input type="text" name="name" id="id" {...register("name")} />
+        <input
+          type="text"
+          name="name"
+          id="id"
+          {...register("name", { required: true, maxLength: 50 })}
+        />
+        {errors.name?.type === "required" && <p>Name is required</p>}
+        {errors.name?.type === "maxLength" && (
+          <p>Name cannot be more than 50 characters</p>
+        )}
       </div>
       <div>
         <label>Date:</label>
-        <input type="date" name="date" id="date" {...register("date")} />
+        <input
+          type="date"
+          name="date"
+          id="date"
+          {...register("date", { required: true })}
+        />
+        {errors.date?.type === "required" && <p>Date is required</p>}
       </div>
       <div>
         <label>Todo:</label>
-        <input type="text" name="desc" id="desc" {...register("desc")} />
+        <input
+          type="text"
+          name="desc"
+          id="desc"
+          {...register("desc", { required: true, maxLength: 200 })}
+        />
+        {errors.desc?.type === "required" && <p>Name is required</p>}
+        {errors.desc?.type === "maxLength" && (
+          <p>Description cannot be more than 50 characters</p>
+        )}
       </div>
       <button>Create</button>
     </form>
